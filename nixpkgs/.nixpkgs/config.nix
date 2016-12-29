48c9efb014173cc47d976a506520743419987e9e
Loïc Minaudier <loic.minaudier@gmail.com>
2016-12-29 14:58:35 +0100

    Add nix pkgs configuration file

---
+++ nixpkgs/.nixpkgs/config.nix
@@ -0,0 +1,31 @@
+{
+  packageOverrides = pkgs_: with pkgs_; {
+    all = with pkgs; buildEnv {
+      name = "all";
+      paths = [
+        fira-mono
+        anonymousPro
+        fzf
+        ranger
+        vlc
+        compton
+        xorg.xbacklight
+        xsel
+        ag
+        htop
+        tree
+        stow
+        vim
+        git
+        gitAndTools.hub
+        termite
+        google-chrome
+        feh
+        font-awesome-ttf
+        i3blocks-gaps
+        direnv
+        spotify
+      ];
+    };
+  };
+}
